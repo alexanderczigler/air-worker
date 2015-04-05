86562e58b91f3726b3c2fc0764ddca3fb4c7e486
ilix <me@ilix.se>
2015-04-05 21:43:31 +0200

    Fixed tab spacing.

--- src/esclient.js
+++ src/esclient.js
@@ -1,20 +1,24 @@
 var elasticsearch = require('elasticsearch');
 var config = require('./air.config.json');
 var client = new elasticsearch.Client({
-    host: config.elasticsearch.host + ':' + config.elasticsearch.port,
-    log: 'trace'
+  host: config.elasticsearch.host + ':' + config.elasticsearch.port,
+  log: 'trace'
 });
 
 module.exports = {
-	save: function (log) {
-		console.log('save', log);
-		client.create({
-        index: config.elasticsearch.index,
-        type: 'reading',
-        id: log.id,
-        body: log
+  save: function (log) {
+    log.date = log.Date;
+    client.index({
+      index: config.elasticsearch.index,
+      type: 'reading',
+      body: log
     }, function (error, response) {
+      if (error) {
         console.log('Error in esclient.save', error, response);
+      }
+      else {
+        console.log('OK', response);
+      }
     });
-	}
+  }
 };

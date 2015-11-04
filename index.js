2bd8ee3762e9cfe58d0d347bae28438307b7afb3
ilix <me@ilix.se>
2015-11-04 21:38:39 +0100

    More restructuring...

--- index.js
+++ index.js
@@ -5,24 +5,10 @@ var q = require('q');
 var worker = require('./lib/worker')
 
 var __intervalId = 0;
-var __processQueue = [];
-
-var moveLog = function(key, callback) {
-  // S3_PATH_UPLOAD
-  // S3_PATH_ARCHIVE
-  var dest = key.replace('new/', 'imported/');
-  s3client.moveLog(key, dest)
-  .catch(function (error) {
-    console.log('Unable to move %key% (%message%)'
-      .replace('%key%', key)
-      .replace('%message%', error.message)
-    );
-  })
-  .done(callback);
-};
+var __queue = [];
 
 var handleLogs = function () {
-  __processQueue.map(function (logMeta) {
+  __queue.map(function (logMeta) {
     s3client.getLog(logMeta.Key)
       .then(function (log) {
         log = JSON.parse(log);
@@ -32,23 +18,37 @@ var handleLogs = function () {
           .then(function () {
             moveLog(logMeta.Key, function () {
               console.log('Moved %key% to imported/'.replace('%key%', logMeta.Key));
-              __processQueue.splice(logMeta);
+              __queue.splice(logMeta);
             });
           })
           .catch(function (error) {
             console.log('Could not save log %key% in ElastcSearch'
               .replace('%key%', logMeta.Key));
-            __processQueue.splice(logMeta);
+            __queue.splice(logMeta);
           });
       })
-      .catch(function (error) { 
+      .catch(function (error) {
         console.log('Could not get log %key% (%message%)'
           .replace('%key%', logMeta.Key)
           .replace('%message%', error));
-        __processQueue.splice(logMeta);
+        __queue.splice(logMeta);
       });
   });
 };
+
+var traverseQueue = () => {
+  __queue.map(key => {
+    console.log(' => Process', key)
+    worker.get(key).then(log => {
+      // Save to ES
+      log = JSON.parse(log);
+      delete log.date;
+      delete log.time;
+
+      return
+    })
+  })
+}
 
 console.log('## air-worker started');
 
@@ -59,11 +59,15 @@ __intervalId = setInterval((function intervalProcess () {
 
   // Add logs to queue.
   worker.list().then(logs => {
-    console.log('got', logs)
+    logs.map(log => {
+      if (__queue.indexOf(log.Key) === -1) {
+        __queue.push(log.Key)
+      }
+    })
   })
 
   // Work the queue.
-  // TODO: actually work the queue.
+  traverseQueue()
 
   return intervalProcess;
 })(), process.env.INTERVAL)
